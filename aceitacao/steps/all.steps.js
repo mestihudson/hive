dc2d9c0aefdb23aa48ea760701c8b2b813475fd2
mestihudson <mestihudson@gmail.com>
2021-02-27 18:19:30 -0300

    test(aceitação): adiciona código de execução aos passos do cenário escolhido

---
+++ aceitacao/steps/all.steps.js
@@ -0,0 +1,13 @@
+const { Given, When, Then } = require('@cucumber/cucumber')
+
+Given(`eu sou um visitante`, () => {
+  return `pending`
+})
+
+When(`eu me registro`, () => {
+  return `pending`
+})
+
+Then(`eu sou um usuário`, () => {
+  return `pending`
+})
