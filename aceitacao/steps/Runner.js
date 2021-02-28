67c574b975203b2ed467996af00799dbf2ed10e7
mestihudson <mestihudson@gmail.com>
2021-02-28 10:56:10 -0300

    test(aceitação): adiciona implementação ao runner

---
+++ aceitacao/steps/Runner.js
@@ -0,0 +1,53 @@
+const { Builder, By } = require('selenium-webdriver')
+const { expect } = require('chai')
+
+class Runner {
+  constructor () {}
+
+  async abrir () {
+    if (!this.driver) {
+      this.driver = new Builder()
+        .forBrowser("chrome")
+        .usingServer("http://hub:4444/wd/hub")
+        .build()
+    }
+    await this.driver.get("http://app:7000")
+  }
+
+  async registro () {
+    const trigger = await this.driver.findElement(
+      By.xpath("//*[@data-trigger='Registro']")
+    )
+    await trigger.click()
+  }
+
+  async preencher (email, senha) {
+    const inputEmail = await this.driver.findElement(
+      By.xpath("//*[@data-input='Email']")
+    )
+    await input.sendKeys(email)
+    const inputSenha = await this.driver.findElement(
+      By.xpath("//*[@data-input='Senha']")
+    )
+    await inputSenha.sendKeys(senha)
+  }
+
+  async registrar () {
+    const trigger = await this.driver.findElement(
+      By.xpath("//*[@data-trigger='Registrar']")
+    )
+    await trigger.click()
+  }
+
+  async registrado (email) {
+    const components = await this.driver.findElements(
+      By.xpath("//*[@data-component='Mensagem']")
+    )
+    for(let component of components) {
+      expect(await component.getText())
+        .to.be(`Usuário ${email} registrado com sucesso`)
+    }
+  }
+}
+
+module.exports = Runner
