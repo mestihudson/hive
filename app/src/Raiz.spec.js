2245599fdd8cbad24981f44bb6e27033a1e3e81a
mestihudson <mestihudson@gmail.com>
2021-03-06 10:51:39 -0300

    test(unidade): adiciona especificação de funcionalidade

--- app/src/Raiz.spec.js
+++ app/src/Raiz.spec.js
@@ -1,3 +1,10 @@
+import { render, screen } from "@testing-library/react"
+
+import Raiz from "./Raiz"
+
 describe("<Raiz/>", () => {
-  it("deve apresentar título da aplicação", () => {})
+  it("deve apresentar título da aplicação", () => {
+    render(<Raiz/>)
+    expect(screen.getByTestId("app-header")).toHaveTextContent("Hive")
+  })
 })
