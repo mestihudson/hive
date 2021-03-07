e0480dc4cceeccd6abfbb9cd994fc2a29c21d906
mestihudson <mestihudson@gmail.com>
2021-03-07 20:07:18 -0300

    refactor(test): altera forma de reutilização da rederização

--- app/src/Raiz.spec.js
+++ app/src/Raiz.spec.js
@@ -4,19 +4,21 @@ import { MemoryRouter as Router } from "react-router-dom"
 import Raiz from "./Raiz"
 
 describe("<Raiz/>", () => {
-  beforeEach(() => {
+  const renderRaiz = () => {
     render(
       <Router>
         <Raiz/>
       </Router>
     )
-  })
+  }
 
   it("deve apresentar título da aplicação", () => {
+    renderRaiz()
     expect(screen.getByTestId("app-header")).toHaveTextContent("Hive")
   })
 
   it("deve apresentar link para página de registro", () => {
+    renderRaiz()
     expect(screen.getByTestId("link-registro")).toHaveTextContent("Registro")
   })
 })
