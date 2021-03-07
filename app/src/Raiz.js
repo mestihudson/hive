176d32597696ac6fc5b528f7aff7e9a7c20eabbf
mestihudson <mestihudson@gmail.com>
2021-03-07 20:28:58 -0300

    fix(app): corrige incapacidade de renderizar componente link fora de router da lib react-router-dom

--- app/src/Raiz.js
+++ app/src/Raiz.js
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link, BrowserRouter as Router } from "react-router-dom"
 
 function Raiz() {
   return (
     <>
       <h1 data-testid="app-header">Hive</h1>
-      <Link to="/registro" data-trigger="Registro">Registro</Link>
+      <Router>
+        <Link to="/registro" data-trigger="Registro">Registro</Link>
+      </Router>
     </>
   )
 }
