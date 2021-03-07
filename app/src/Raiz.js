2125ce252dc60e9640e38312512403b0ba133e8d
mestihudson <mestihudson@gmail.com>
2021-03-07 19:48:35 -0300

    feat: adiciona funcionalidade

--- app/src/Raiz.js
+++ app/src/Raiz.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom"
+
 function Raiz() {
   return (
     <>
       <h1 data-testid="app-header">Hive</h1>
+      <Link to="/registro" data-testid="link-registro">Registro</Link>
     </>
   )
 }
