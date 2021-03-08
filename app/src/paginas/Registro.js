3ebdedd1242250a035abccb50a8e5342b40e22a3
mestihudson <mestihudson@gmail.com>
2021-03-07 21:33:52 -0300

    feat: adiciona funcionalidade

---
+++ app/src/paginas/Registro.js
@@ -0,0 +1,12 @@
+function Registro() {
+  return (
+    <>
+      <h2 data-testid="page-header">Registro</h2>
+      <input type="text" data-input="Email"/>
+      <input type="password" data-input="Senha"/>
+      <button data-trigger="Registrar">Registrar</button>
+    </>
+  )
+}
+
+export default Registro
