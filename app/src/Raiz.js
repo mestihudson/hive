da4bfa77616e70e6269adf8638aaad1dddb32d5c
mestihudson <mestihudson@gmail.com>
2021-03-07 21:38:21 -0300

    feat: adiciona página ao roteador do componente raiz da aplicação

--- app/src/Raiz.js
+++ app/src/Raiz.js
@@ -1,4 +1,6 @@
-import { Link, BrowserRouter as Router } from "react-router-dom"
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+import Registro from "./paginas/Registro"
 
 function Raiz() {
   return (
@@ -6,6 +8,11 @@ function Raiz() {
       <h1 data-testid="app-header">Hive</h1>
       <Router>
         <Link to="/registro" data-trigger="Registro">Registro</Link>
+        <Switch>
+          <Route>
+            <Registro/>
+          </Route>
+        </Switch>
       </Router>
     </>
   )
