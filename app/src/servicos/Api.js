8bc43be8d41bf12aa0d51b8c94e514bc7bf49317
mestihudson <mestihudson@gmail.com>
2021-03-22 16:22:45 -0300

    feat: adiciona chamada a api via axios

--- app/src/servicos/Api.js
+++ app/src/servicos/Api.js
@@ -1,5 +1,13 @@
+import axios from "axios"
+
+axios.defaults.baseURL = "/api"
+
 const Api = {
-  registrar (payload) {}
+  registrar (payload) {
+    console.error(payload)
+    return axios.post("/usuarios", payload)
+      .then((result) => Promise.resolve(result))
+  }
 }
 
 export default Api
