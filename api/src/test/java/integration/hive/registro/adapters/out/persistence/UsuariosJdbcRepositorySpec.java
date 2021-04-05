4c1f90db408143a8333c9a4f2969cb266d28bb7c
mestihudson <mestihudson@gmail.com>
2021-04-05 17:00:42 -0300

    test: insere registro na tabela usuários

--- api/src/test/java/integration/hive/registro/adapters/out/persistence/UsuariosJdbcRepositorySpec.java
+++ api/src/test/java/integration/hive/registro/adapters/out/persistence/UsuariosJdbcRepositorySpec.java
@@ -9,16 +9,20 @@ import io.quarkus.test.junit.QuarkusTest;
 
 import javax.inject.Inject;
 
+import org.assertj.db.type.*;
 import org.junit.jupiter.api.*;
 
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.db.api.Assertions.assertThat;
 
 
 @QuarkusTest
 class UsuariosJdbcRepositorySpec {
   UsuariosJdbcRepository repository;
   @Inject AgroalDataSource ds;
+  Changes changes;
+  final Long ID = 1L;
   final String EMAIL = "usuario@meuemail.com";
   final String SENHA = "senha";
 
@@ -32,6 +36,27 @@ class UsuariosJdbcRepositorySpec {
     insertUsuario();
     assertThatExceptionOfType(EmailVinculadoException.class)
       .isThrownBy(() -> { repository.validar(EMAIL); });
+  }
+
+  @Test void deveInserirUmRegistroNaTabelaUsuarios() throws Throwable {
+    cleandb();
+    changes = new Changes(ds);
+    changes.setStartPointNow();
+
+    final UsuariosJdbcRepository repository = new UsuariosJdbcRepository(ds);
+    repository.criarConta(
+      Usuario.builder().email(EMAIL).senha(SENHA).build()
+    );
+
+    changes.setEndPointNow();
+
+    assertThat(changes)
+      .change()
+        .rowAtStartPoint()
+          .doesNotExist()
+        .rowAtEndPoint()
+          .exists()
+          .hasValues(ID, EMAIL, SENHA);
   }
 
   void execute(final String query) {
