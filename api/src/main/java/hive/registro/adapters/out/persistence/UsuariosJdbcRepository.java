72239586dd5b28ccd440f0108301294bef2cf5bd
mestihudson <mestihudson@gmail.com>
2021-04-05 16:59:48 -0300

    feat: dispara excecao quando email já vinculado

---
+++ api/src/main/java/hive/registro/adapters/out/persistence/UsuariosJdbcRepository.java
@@ -0,0 +1,40 @@
+package hive.registro.adapters.out.persistence;
+
+
+import hive.registro.application.port.out.*;
+import hive.registro.domain.*;
+
+import io.agroal.api.AgroalDataSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+
+
+public class UsuariosJdbcRepository
+  implements EmailVinculadoValidator, CriarContaPort {
+  private final AgroalDataSource ds;
+
+  @Inject public UsuariosJdbcRepository(final AgroalDataSource ds) {
+    this.ds = ds;
+  }
+
+  @Override
+  public void validar(final String valor) {
+    try {
+      final ResultSet rs = ds.getConnection().createStatement().executeQuery(
+        String.format(
+          "select count(id) as quant from usuarios where email = '%s'",
+          valor
+        )
+      );
+      if (rs.next() && rs.getString("quant").equals("1")) {
+        throw new EmailVinculadoException();
+      }
+    } catch (SQLException sqle) {
+      throw new DomainException(sqle);
+    }
+  }
+
+}
