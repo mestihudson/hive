31da6b7583cfd52bd0226c00a7afbdcf64616beb
mestihudson <mestihudson@gmail.com>
2021-04-05 15:25:36 -0300

    feat: criar conta resource

--- api/src/main/java/hive/registro/adapters/in/web/CriarContaResource.java
+++ api/src/main/java/hive/registro/adapters/in/web/CriarContaResource.java
@@ -1,6 +1,35 @@
 package hive.registro.adapters.in.web;
+
+
+import hive.registro.application.port.in.*;
+import hive.registro.domain.*;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
 import lombok.*;
+
+
+@Path("criar-conta")
 public class CriarContaResource {
+  private final CriarContaUseCase uc;
+
+  @Inject public CriarContaResource(final CriarContaUseCase uc) {
+    this.uc = uc;
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response criarConta(final Request request) {
+    try {
+      uc.criarConta(request.toCommand());
+      return Response.status(201).build();
+    } catch (DomainException de) {
+      return Response.status(400).entity(de.getMessage()).build();
+    }
+  }
+
   @AllArgsConstructor
   @NoArgsConstructor
   public static class Request {
